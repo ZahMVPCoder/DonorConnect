58f7949437909a7a888f05ff407ed486d06b55af
Zahir Alston <zalst0056@launchpadphilly.org>
2026-01-07 11:34:24 -0500

    Fix Prisma client syntax error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
-  });
+  return new PrismaClient();
 };
 
 declare global {
   var prisma: PrismaClient | undefined;
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
